5219c25de030566e31a5aaaf480fd82c1b7a6368
warcooft <syamsuddine9@gmail.com>
2024-08-30 11:08:26 +0700

    initial commit

---
+++ Config/MultiEmail.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) 2024 CodeIgniter Foundation <syamsuddine9@icloud.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Aselsan\CodeigniterMultiSmtpConfig\Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class MultiEmail extends BaseConfig
+{
+    public string $defaultGroup = 'default';
+
+    public array $default = [
+        'fromEmail'     => '',
+        'fromName'      => '',
+        'recipients'    => '',
+        'userAgent'     => 'CodeIgniter',
+        'protocol'      => 'smtp',
+        'mailPath'      => '/usr/sbin/sendmail',
+        'SMTPHost'      => 'smtp.gmail.com',
+        'SMTPUser'      => '',
+        'SMTPPass'      => '',
+        'SMTPPort'      => 587,
+        'SMTPTimeout'   => 30,
+        'SMTPKeepAlive' => false,
+        'SMTPCrypto'    => 'tls',
+        'wordWrap'      => true,
+        'wrapChars'     => 76,
+        'mailType'      => 'html',
+        'charset'       => 'UTF-8',
+        'validate'      => false,
+        'priority'      => 3,
+        'CRLF'          => "\r\n",
+        'newline'       => "\r\n",
+        'BCCBatchMode'  => false,
+        'BCCBatchSize'  => 200,
+        'DSN'           => false,
+    ];
+
+    public array $outlook = [
+        'fromEmail'     => '',
+        'fromName'      => '',
+        'recipients'    => '',
+        'userAgent'     => 'CodeIgniter',
+        'protocol'      => 'smtp',
+        'mailPath'      => '/usr/sbin/sendmail',
+        'SMTPHost'      => 'smtp.office365.com',
+        'SMTPUser'      => '',
+        'SMTPPass'      => '',
+        'SMTPPort'      => 587,
+        'SMTPTimeout'   => 30,
+        'SMTPKeepAlive' => false,
+        'SMTPCrypto'    => 'tls',
+        'wordWrap'      => true,
+        'wrapChars'     => 76,
+        'mailType'      => 'html',
+        'charset'       => 'UTF-8',
+        'validate'      => false,
+        'priority'      => 3,
+        'CRLF'          => "\r\n",
+        'newline'       => "\r\n",
+        'BCCBatchMode'  => false,
+        'BCCBatchSize'  => 200,
+        'DSN'           => false,
+    ];
+
+    public array $mailgun = [
+        'fromEmail'     => '', // Replace it with the email you used as the sender
+        'fromName'      => '', // Replace with sender name
+        'recipients'    => '', // Can be left blank if there are no default settings for the recipient
+        'userAgent'     => 'CodeIgniter',
+        'protocol'      => 'smtp',
+        'mailPath'      => '/usr/sbin/sendmail',
+        'SMTPHost'      => 'smtp.mailgun.org', // Host SMTP Mailgun
+        'SMTPUser'      => '', // Replace with your Mailgun email address (e.g. postmaster@yourdomain.com)
+        'SMTPPass'      => '', // Replace it with the Mailgun SMTP password or API key
+        'SMTPPort'      => 587, // Port SMTP Mailgun for TLS
+        'SMTPTimeout'   => 30,
+        'SMTPKeepAlive' => false,
+        'SMTPCrypto'    => 'tls', // Encryption TLS untuk Mailgun
+        'wordWrap'      => true,
+        'wrapChars'     => 76,
+        'mailType'      => 'html',
+        'charset'       => 'UTF-8',
+        'validate'      => false,
+        'priority'      => 3,
+        'CRLF'          => "\r\n",
+        'newline'       => "\r\n",
+        'BCCBatchMode'  => false,
+        'BCCBatchSize'  => 200,
+        'DSN'           => false,
+    ];
+
+    public array $sendgrid = [
+        'fromEmail'     => '', // Replace it with the email you used as the sender
+        'fromName'      => '', // Replace with sender name
+        'recipients'    => '', // Can be left blank if there are no default settings for the recipient
+        'userAgent'     => 'CodeIgniter',
+        'protocol'      => 'smtp',
+        'mailPath'      => '/usr/sbin/sendmail',
+        'SMTPHost'      => 'smtp.sendgrid.net', // Host SMTP Mailgun
+        'SMTPUser'      => '', // Username SMTP SendGrid, use 'apikey'
+        'SMTPPass'      => '', // Replace it with the SendGrid SMTP password or API key
+        'SMTPPort'      => 587, // Port SMTP Mailgun for TLS
+        'SMTPTimeout'   => 30,
+        'SMTPKeepAlive' => false,
+        'SMTPCrypto'    => 'tls', // Encryption TLS for SendGrid
+        'wordWrap'      => true,
+        'wrapChars'     => 76,
+        'mailType'      => 'html',
+        'charset'       => 'UTF-8',
+        'validate'      => false,
+        'priority'      => 3,
+        'CRLF'          => "\r\n",
+        'newline'       => "\r\n",
+        'BCCBatchMode'  => false,
+        'BCCBatchSize'  => 200,
+        'DSN'           => false,
+    ];
+}
---
+++ src/Helpers/multi_email_helper.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter Shield.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+use CodeIgniter\Email\Email;
+
+if (! defined('multi_email')) {
+    /**
+     * Provides convenient access to the CodeIgniter Email class.
+     *
+     * @param array<string, mixed> $overrides Email preferences to override.
+     *
+     * @internal
+     */
+    function multi_email(array $overrides = [], string $group = ''): Email
+    {
+        if (!setting('MultiEmail.' . $group)) {
+            throw new Exception("Cannot send email from 'multi_email' helper.\n Undefined group name:  $group");
+        }
+
+        $defaultGroup  = !empty($group) ? $group : setting('MultiEmail.defaultGroup');
+
+        $config = [
+            'fromEmail'     => setting('MultiEmail.' . $defaultGroup)['fromEmail'],
+            'fromName'      => setting('MultiEmail.' . $defaultGroup)['fromName'],
+            'userAgent'     => setting('MultiEmail.' . $defaultGroup)['userAgent'],
+            'protocol'      => setting('MultiEmail.' . $defaultGroup)['protocol'],
+            'mailPath'      => setting('MultiEmail.' . $defaultGroup)['mailPath'],
+            'SMTPHost'      => setting('MultiEmail.' . $defaultGroup)['SMTPHost'],
+            'SMTPUser'      => setting('MultiEmail.' . $defaultGroup)['SMTPUser'],
+            'SMTPPass'      => setting('MultiEmail.' . $defaultGroup)['SMTPPass'],
+            'SMTPPort'      => setting('MultiEmail.' . $defaultGroup)['SMTPPort'],
+            'SMTPTimeout'   => setting('MultiEmail.' . $defaultGroup)['SMTPTimeout'],
+            'SMTPKeepAlive' => setting('MultiEmail.' . $defaultGroup)['SMTPKeepAlive'],
+            'SMTPCrypto'    => setting('MultiEmail.' . $defaultGroup)['SMTPCrypto'],
+            'wordWrap'      => setting('MultiEmail.' . $defaultGroup)['wordWrap'],
+            'wrapChars'     => setting('MultiEmail.' . $defaultGroup)['wrapChars'],
+            'mailType'      => setting('MultiEmail.' . $defaultGroup)['mailType'],
+            'charset'       => setting('MultiEmail.' . $defaultGroup)['charset'],
+            'validate'      => setting('MultiEmail.' . $defaultGroup)['validate'],
+            'priority'      => setting('MultiEmail.' . $defaultGroup)['priority'],
+            'CRLF'          => setting('MultiEmail.' . $defaultGroup)['CRLF'],
+            'newline'       => setting('MultiEmail.' . $defaultGroup)['newline'],
+            'BCCBatchMode'  => setting('MultiEmail.' . $defaultGroup)['BCCBatchMode'],
+            'BCCBatchSize'  => setting('MultiEmail.' . $defaultGroup)['BCCBatchSize'],
+            'DSN'           => setting('MultiEmail.' . $defaultGroup)['DSN'],
+        ];
+
+        if ($overrides !== []) {
+            $config = array_merge($config, $overrides);
+        }
+
+        /** @var Email $email */
+        $email = service('email');
+
+        return $email->initialize($config);
+    }
+}
