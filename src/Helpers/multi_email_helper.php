68ecb90657683fa773ad6dc9db41b2eb1df68a1c
warcooft <syamsuddine9@gmail.com>
2024-08-30 12:29:36 +0700

    update docs

--- src/Helpers/multi_email_helper.php
+++ src/Helpers/multi_email_helper.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 /**
- * This file is part of CodeIgniter Shield.
+ * This file is part of CodeIgniter 4 framework.
  *
- * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ * (c) CodeIgniter Foundation <syamsuddine9@icloud.com>
  *
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
